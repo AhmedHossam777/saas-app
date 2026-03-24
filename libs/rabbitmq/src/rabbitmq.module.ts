cfdbb76113e2ef00f9a7e8d7eb7a6b878c9ac860
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-24 18:40:07 +0200

    i

--- libs/rabbitmq/src/rabbitmq.module.ts
+++ libs/rabbitmq/src/rabbitmq.module.ts
@@ -9,6 +9,7 @@ import { RabbitmqService } from './rabbitmq.service';
 export interface RabbitmqModuleOptions {
   name: string;
   queue: string;
+  url?: string;
 }
 
 @Module({})
@@ -17,22 +18,26 @@ export class RabbitmqModule {
     return {
       module: RabbitmqModule,
       imports: [
-        ClientsModule.registerAsync([
+        ClientsModule.register([
           {
             name: options.name,
-            useFactory: () => ({
-              transport: Transport.RMQ,
-              options: {
-                urls: [process.env.RABBITMQ_URL ?? 'amqp://saas:saas_secret@rabbitmq:5672'],
-                queue: options.queue,
-                queueOptions: { durable: true },
-              },
-            } as RmqOptions),
+            transport: Transport.RMQ,
+            options: {
+              urls: [options.url ?? 'amqp://saas:saas_secret@rabbitmq:5672'],
+              queue: options.queue,
+              queueOptions: { durable: true },
+              noAck : true,
+            },
           },
         ]),
       ],
       providers: [RabbitmqService],
       exports: [ClientsModule, RabbitmqService],
     };
+  }
+
+
+  static registerAsync (options: RabbitmqModuleOptions): DynamicModule{
+    return this.register(options)
   }
 }
--- libs/rabbitmq/src/rabbitmq.service.ts
+++ libs/rabbitmq/src/rabbitmq.service.ts
@@ -1,17 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { RmqContext } from '@nestjs/microservices';
 
 @Injectable()
 export class RabbitmqService {
+  private readonly logger = new Logger(RabbitmqService.name)
   ack(context: RmqContext) {
     const channel = context.getChannelRef();
     const message = context.getMessage();
     channel.ack(message);
+    this.logger.debug('Message acked');
   }
 
   nack(context: RmqContext, requeue = false) {
     const channel = context.getChannelRef();
     const message = context.getMessage();
     channel.nack(message, false, requeue);
+    this.logger.debug('Message nacked');
   }
 }
