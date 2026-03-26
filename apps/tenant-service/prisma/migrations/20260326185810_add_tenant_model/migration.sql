99ff39740e865d4839cd41e14d4720dcc7060523
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-03-26 20:58:26 +0200

    feat(tenant): add tenant model

---
+++ apps/tenant-service/prisma/migrations/20260326185810_add_tenant_model/migration.sql
@@ -0,0 +1,71 @@
+-- CreateEnum
+CREATE TYPE "TenantType" AS ENUM ('PERSONAL', 'ORGANIZATION');
+
+-- CreateEnum
+CREATE TYPE "MemberRole" AS ENUM ('OWNER', 'ADMIN', 'MEMBER');
+
+-- CreateTable
+CREATE TABLE "tenants" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "TenantType" NOT NULL DEFAULT 'ORGANIZATION',
+    "slug" TEXT NOT NULL,
+    "avatar_url" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tenants_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "memberships" (
+    "id" TEXT NOT NULL,
+    "tenantId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "role" "MemberRole" NOT NULL DEFAULT 'MEMBER',
+    "joined_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "memberships_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "invitations" (
+    "id" TEXT NOT NULL,
+    "tenant_id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "MemberRole" NOT NULL DEFAULT 'MEMBER',
+    "token" TEXT NOT NULL,
+    "invited_by" TEXT NOT NULL,
+    "expires_at" TIMESTAMP(3) NOT NULL,
+    "accepted_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "invitations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tenants_slug_key" ON "tenants"("slug");
+
+-- CreateIndex
+CREATE INDEX "tenants_slug_idx" ON "tenants"("slug");
+
+-- CreateIndex
+CREATE INDEX "memberships_tenantId_idx" ON "memberships"("tenantId");
+
+-- CreateIndex
+CREATE INDEX "memberships_userId_idx" ON "memberships"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "memberships_userId_tenantId_key" ON "memberships"("userId", "tenantId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "invitations_token_key" ON "invitations"("token");
+
+-- CreateIndex
+CREATE INDEX "invitations_token_idx" ON "invitations"("token");
+
+-- AddForeignKey
+ALTER TABLE "memberships" ADD CONSTRAINT "memberships_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "tenants"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "invitations" ADD CONSTRAINT "invitations_tenant_id_fkey" FOREIGN KEY ("tenant_id") REFERENCES "tenants"("id") ON DELETE CASCADE ON UPDATE CASCADE;
